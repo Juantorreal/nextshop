6486a6dabcecd14b74bc37c90eb2b66e1d3c9d3e
Juan Torreal <juantorreal@gmail.com>
2022-09-24 16:44:48 -0300

    globalStyles

---
+++ next/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import {styled} from "../styles"
+
+const Button = styled('button',
+{backgroundColor: '$green500'
+}
+)
+
+export default function Home() {
+  return (
+   <Button>Hello World</Button>
+  )
+}
