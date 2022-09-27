9112fb7aef967a5d9766a5c833238f0888a25d47
Juan Torreal <juantorreal@gmail.com>
2022-09-26 23:44:56 -0300

    home

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,4 +1,10 @@
+import Image from "next/future/image"
 import {styled} from "../styles"
+import { HomeContainer, Product } from "./home"
+
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 const Button = styled('button',
 {backgroundColor: '$green500'
@@ -7,6 +13,10 @@ const Button = styled('button',
 
 export default function Home() {
   return (
-   <Button>Hello World</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480}/>
+      </Product>
+    </HomeContainer>
   )
 }
