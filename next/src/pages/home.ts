5a651ecc016349733f65e9597dca8098ce28a7f2
Juan Torreal <juantorreal@gmail.com>
2022-10-01 18:44:57 -0300

    carrossel

--- next/src/pages/home.ts
+++ next/src/pages/home.ts
@@ -2,7 +2,7 @@ import { styled } from "../styles";
 
 export const HomeContainer = styled('main',{
 display: 'flex',
-gap: '3rem',
+//gap: '3rem',
 maxWidth: 'calc(100vw - ((100vw - 1180px)/2))',
 marginLeft: 'auto',
 minHeight: 656,
@@ -11,7 +11,7 @@ minHeight: 656,
 export const Product = styled('a', {
     background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
     borderRadius: 8,
-    padding: '0.25rem',
+    //padding: '0.25rem',
     cursor:'pointer',
     position: 'relative',
     display: 'flex',
--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Image from "next/future/image"
 import {styled} from "../styles"
 import { HomeContainer, Product } from "./home"
+import {useKeenSlider} from "keen-slider/react"
 
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
+import 'keen-slider/keen-slider.min.css'
 
 const Button = styled('button',
 {backgroundColor: '$green500'
@@ -12,22 +14,38 @@ const Button = styled('button',
 )
 
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView:3,
+      spacing: 48,
+
+    }
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image alt='' src={camiseta1} width={520} height={480}/>
         <footer>
           <strong>Camiseta</strong>
           <span>R$ 80,00</span>
         </footer>
       </Product>
-      <Product>
-        <Image alt='' src={camiseta1} width={520} height={480}/>
+      <Product className="keen-slider__slide">
+        <Image alt='' src={camiseta2} width={520} height={480}/>
         <footer>
           <strong>Camiseta</strong>
           <span>R$ 80,00</span>
         </footer>
       </Product>
+      <Product className="keen-slider__slide">
+        <Image alt='' src={camiseta3} width={520} height={480}/>
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 80,00</span>
+        </footer>
+      </Product>
+
       
     </HomeContainer>
   )
